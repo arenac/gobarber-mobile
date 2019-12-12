286852a8ac6b90f7601a4346af71397d311c48e2
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-12 22:55:27 +0100

    redux and saga debug configs

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '192.168.2.105' }) // to connect android add { host: 'ip'}
+    .setAsyncStorageHandler(AsyncStorage)
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
