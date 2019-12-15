ef33a07786422003ad9bab6ce10535b61c5ae74d
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-15 21:34:16 +0100

    app remane with react-native-rename

--- ios/gobarberTests/gobarberTests.m
+++ ios/gobarberTests/gobarberTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface gympointTests : XCTestCase
+@interface gobarberTests : XCTestCase
 
 @end
 
-@implementation gympointTests
+@implementation gobarberTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
