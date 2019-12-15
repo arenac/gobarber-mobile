9f0caa24b8ffe2e12e17e3b6f51dc0da1e737a8e
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-15 22:38:02 +0100

    signup with button loading

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import logo from '~/assets/logo.png';
@@ -15,11 +16,20 @@ import {
   SignInLinkText,
 } from './styles';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 export default function SignUp({ navigation }) {
   const emailRef = useRef();
   const passwordRef = useRef();
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const loading = useSelector(state => state.auth.loading);
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -33,6 +43,8 @@ export default function SignUp({ navigation }) {
             placeholder="Full name"
             returnKeyType="next"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
           <FormInput
             icon="mail-outline"
@@ -43,6 +55,8 @@ export default function SignUp({ navigation }) {
             ref={emailRef}
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -53,9 +67,13 @@ export default function SignUp({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Sign Up</SubmitButton>
+          <SubmitButton onPress={handleSubmit} loading={loading}>
+            Sign Up
+          </SubmitButton>
         </Form>
         <SignInLink
           onPress={() => {
