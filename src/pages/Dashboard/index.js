49f017f8920a3891581971aa19c7185ed8330964
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-15 23:24:43 +0100

    Dashboard tab lable and icon

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <Text>Dashboard</Text>;
+  return (
+    <Background>
+      <Text>Dashboard</Text>
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Dashboard',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="dashboard" size={20} color={tintColor} />
+  ),
+};
