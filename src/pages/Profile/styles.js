4dd5cd2aff493ff05815a37a98b8aa0f05e31d99
Nilo Neregato <nilo.neregato@gmail.com>
2019-12-17 13:30:16 +0100

    styling button

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -26,12 +26,17 @@ export const FormInput = styled(Input)`
   margin-bottom: 10px;
 `;
 
-export const SubmitButton = styled(Button)`
-  margin-top: 5px;
-`;
-
 export const Separator = styled.View`
   height: 1px;
   background: rgba(255, 255, 255, 0.2);
   margin: 20px 0 30px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
+
+export const LogoutButton = styled(Button)`
+  margin-top: 10px;
+  background: #f64c75;
 `;
